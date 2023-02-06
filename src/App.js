742ded53063b56ce209a25acde864e9ff182095a
Koray Cetin <korayctnx06@gmail.com>
2023-02-06 19:40:30 +0300

    header is done

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Menu from './components/Menu'
 import Home from './components/Home'
 import Contact from './components/Contact'
 import About from './components/About'
+import Navbar from './components/Navbar';
 
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/menu' element={<Menu/>}/>
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom';
+import logo from '../assets/burgerlogo.png'
+import css from '../styles/Navbar.css'
+function Navbar() {
+  return (
+    <div className='navbar'>
+
+            <img src={logo} alt="BurgerYiyelim Logo" className='logo'/>
+            <div className='nav-links'>
+                <NavLink to='/'>Homepage</NavLink>
+                <NavLink to='/menu'>Menu</NavLink>
+                <NavLink to='/about'>About</NavLink>
+                <NavLink to='/contact'>Contact</NavLink>
+            </div>
+        
+    </div>
+  )
+}
+
+export default Navbar
